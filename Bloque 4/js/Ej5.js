1f60d8f0929f7f8cd5ae9ddc49355033a5fa1609
agarcle1210 <andgarcle26@gmail.com>
2025-01-27 10:49:35 +0100

    ejercicio 5

--- Bloque 4/js/Ej5.js
+++ Bloque 4/js/Ej5.js
@@ -1,7 +1,14 @@
-
-
-var num;
-num=Math.random()*3;
-num=parseInt(num); 
-if (num==0)
-window.location='http://www.outlook.com';
+function redireccionar() {
+    let num;
+    num = Math.random() * 3;
+    num = parseInt(num);
+    if (num == 0) {
+        window.location = 'https://www.outlook.com';
+    }
+    if (num == 1) {
+        window.location = 'https://www.yahoo.com';
+    }
+    if (num == 2) {
+        window.location = 'https://www.gmail.com';
+    }
+}
